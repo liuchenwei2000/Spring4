214db1a921871ccbb4c92a8d33bd7b439e3dc9d8
Vic Liu <liuchenwei2000@163.com>
2016-12-22 14:21:43 +0800

    Spring Boot complete

--- spring-integration/spring-boot/src/main/resources/schema.sql
+++ spring-integration/spring-boot/src/main/resources/schema.sql
@@ -1,12 +1,14 @@
 /* 根路径下的 schema.sql 文件会被应用在启动的时候自动加载并执行 */
+
+DROP TABLE boot_book;
+
 CREATE TABLE boot_book
     (
         id VARCHAR(20) NOT NULL,
         name VARCHAR(255),
         author VARCHAR(255),
         PRIMARY KEY (id)
-    )
-    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into boot_book (id, name, author) values ('A0003', 'Effective Java', 'Joshua Bloch');
 insert into boot_book (id, name, author) values ('A0004', 'Practical Java', 'Peter Haggar ');
